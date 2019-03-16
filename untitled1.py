dde5f308f52e5ce0aae22d7ce21f495f6af81894
so2liu <so2liu@gamil.com>
2019-03-16 11:35:11 +0100

    py document from friend

---
+++ untitled1.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 15 23:56:20 2019
+
+@author: Administrator
+"""
+import matplotlib.pyplot as plt
+import scipy.io as sio
+from sklearn.svm import SVR
+import numpy as np
+data = sio.loadmat('088IRWaSS7_Wi1d89_C4d3_wave')
+datensatzen = 2000
+datenlänge =500
+predictlen = 200
+plt.close
+testdatenstart =np.random.randint(10000,80000)
+print(testdatenstart)
+wave = data['WG10_DHI'][0][0][0]
+wave = [i * 10 for i in wave]
+length = np.arange(len(wave))
+#plt.figure(1)
+inputdata= []
+#plt.plot(length,y1)
+for i in range(datensatzen):
+    for l in range (datenlänge):
+        
+        inputdata.append(float(wave[i+l]))
+inputdata = np.array(inputdata).reshape(datensatzen,datenlänge)
+inputlabel = np.zeros(shape =(datensatzen,predictlen))
+for i in range(datensatzen):    
+    for j in range(predictlen):
+        inputlabel[i,j] = wave[i+datenlänge+j]
+xtest = []
+for a in range(datenlänge):
+    xtest.append(float(wave[testdatenstart+a]))
+xtest = np.array(xtest).reshape(1,-1)     
+ypredict = []
+ytest = wave[testdatenstart+datenlänge:testdatenstart+datenlänge+predictlen]
+ytest = [ytest[i]for i in range(0,predictlen,4)]
+for k in range (0,predictlen,4):
+    clf = SVR(kernel = 'rbf', gamma=0.001, C=1000)
+    inputy = inputlabel[:,k]
+    clf.fit(inputdata,inputy)
+    ypredict.append(clf.predict(xtest))
+
+plt.plot(range(len(ytest)),ypredict,color = 'r', label = 'predict')
+plt.plot(range(len(ytest)),ytest,color = 'b', label = 'real')
