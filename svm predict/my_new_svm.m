87ee3f91ec1220af2eb868588beee994730cf44a
dumoon92 <395206732@qq.com>
2019-03-18 16:07:41 +0100

    10 * 10 new svm

--- svm predict/my_new_svm.m
+++ svm predict/my_new_svm.m
@@ -1,21 +1,7 @@
-close all
-clear;
-clc;
-format compact;
-%%
-data=load('088IRWaSS7_Wi1d89_C4d3_wave.mat');
-data=data.WG10_DHI;
-x_raw=data.Time;
-y_raw=data.Data;
+function [test_y, predict_y, error] = my_new_svm(y_raw, data_set_num, train_len, predict_len, start_train_index, start_predict_index)
+%% use front y as x, following y as y, no relation of time
 y = my_row_normalize(y_raw);
 % y = sin(2*pi*linspace(0, 5*pi, 1e3))';  % use sinus function as test
-data_set_num = 3e2;
-train_len = 1e2;
-predict_len = 1000;
-
-start_train_index = 1;
-start_predict_index = 30000;
-% start_predict_index = randi([4e4 8e4],1,1)
 
 train_input = zeros(data_set_num, train_len);
 train_label = zeros(data_set_num, predict_len);
@@ -34,6 +20,7 @@ for label_index = 1:predict_len
     predict_y(:, label_index) = predict(svmMdl, predict_y_input);
 %     size(predict(svmMdl, y(start_predict_index-predict_len+label_index:start_predict_index-1+label_index)))
 end
+figure
 subplot(2, 1, 1);
 plot(predict_y(1, :), 'r--')
 hold on 
@@ -41,10 +28,11 @@ plot(test_y, 'b')
 title({['data set num = ', num2str(data_set_num)]; ['train len = ', num2str(train_len)]; ['predict len = ', num2str(predict_len)]; ...
         ['start train index = ', num2str(start_train_index)]; ['start predict index = ', num2str(start_predict_index)]}');
 subplot(2, 1, 2);
+error = sum(abs(test_y - predict_y(1, :)')./test_y)/numel(test_y);
 plot(abs(test_y - predict_y(1, :)')./test_y);
 hold on 
 title('relative error in %')
-saveas(gcf, strcat('untitled_', regexprep(datestr(datetime('now')), {'[%() :]+', '_+$'}, {'_', ''}), '.pdf'));
+saveas(gcf, strcat('new_svm_', regexprep(datestr(datetime('now')), {'[%() :]+', '_+$'}, {'_', ''}), '.pdf'));
 
 
 
---
+++ svm predict/new_svm.m
@@ -0,0 +1,31 @@
+clear
+close  all
+clc
+data=load('088IRWaSS7_Wi1d89_C4d3_wave.mat');
+data=data.WG10_DHI;
+x_raw=data.Time;
+y_raw=data.Data;
+
+mesh_dencity = 10;
+data_set_num_set = floor(linspace(1e1, 5e2, mesh_dencity));
+train_len_set = floor(linspace(1e1, 5e2, mesh_dencity)); 
+
+predict_len = 100; 
+start_train_index = 1; 
+start_predict_index = 3e4;
+
+error_matrix = ones(mesh_dencity, mesh_dencity);
+for i = 1:numel(data_set_num_set)
+    data_set_num = data_set_num_set(i);
+    for k = 1:numel(train_len_set)
+        train_len = train_len_set(k);
+        [test_y, predict_y, error] = my_new_svm(y_raw, data_set_num, train_len, ...
+            predict_len, start_train_index, start_predict_index);
+        error_matrix(i, k) = error;
+    end
+end
+% h = heatmap(data_set_num_set, train_len_set, error_matrix);
+figure
+h = heatmap(error_matrix);
+saveas(gcf, strcat('new_svm_error_', regexprep(datestr(datetime('now')), {'[%() :]+', '_+$'}, {'_', ''}), '.pdf'));
+save('new_svm.mat')
