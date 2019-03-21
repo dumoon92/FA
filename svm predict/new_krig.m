7a04243180572ee0f1ce7d826f06a8ea96146627
dumoon92 <395206732@qq.com>
2019-03-21 15:12:42 +0100

    add time

--- svm predict/new_krig.m
+++ svm predict/new_krig.m
@@ -24,15 +24,17 @@ krig_kernel = {'squaredexponential', 'matern32', 'matern52',...
     'ardsquaredexponential'};
 kernel_num = numel(krig_kernel);
 error_matrix = ones(mesh_dencity, mesh_dencity, kernel_num);
+time_matrix = ones(mesh_dencity, mesh_dencity, 4);
 for kernel_num = 1:kernel_num
-    
     for i = 1:numel(data_set_num_set)
         data_set_num = data_set_num_set(i);
         for k = 1:numel(train_len_set)
             train_len = train_len_set(k);
+            tic
             [test_y, predict_y, error] = my_new_krig(y_raw, data_set_num, train_len, ...
                 predict_len, start_train_index, start_predict_index, krig_kernel(kernel_num));
-            error_matrix(k, i) = error;
+            error_matrix(k, i, kernel_num) = error;
+            time_matrix(k, i, kernel_num) = toc;
         end
     end
 end
