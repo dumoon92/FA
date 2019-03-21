f148d4074a4d371a168970ed17bc522ed9a54287
dumoon92 <395206732@qq.com>
2019-03-21 11:21:37 +0100

    version adaptive

--- svm predict/new_krig.m
+++ svm predict/new_krig.m
@@ -36,15 +36,16 @@ end
 figure('units','normalized','outerposition',[0 0 1 1])  % output graph as full screen
 for kernel_num = 1:kernel_num
     subplot(2, 2, kernel_num);
-%     h = heatmap(error_matrix(:, :, kernel_num));
-    
-    % only for high version MATLAB
-    h = heatmap(data_set_num_set, train_len_set, error_matrix(:, :, kernel_num));  
-    h.XLabel = 'data set num set';
-    h.YLabel = 'train len set';
-    h.Title = 'Ralative Errors in different parameters';
-    
-    title(strcat('Krig with kernel', krig_kernel(kernel_num)))
+    if strfind(version, '2015')
+        h = heatmap(error_matrix(:, :, kernel_num));
+        title(strcat('Krig with kernel', krig_kernel(kernel_num)))
+    elseif strfind(version, '2018')
+        % only for high version MATLAB
+        h = heatmap(data_set_num_set, train_len_set, error_matrix(:, :, kernel_num));  
+        h.XLabel = 'data set num set';
+        h.YLabel = 'train len set';
+        h.Title = strcat('Ralative Errors by Krig with kernel: ', krig_kernel(kernel_num));
+    end
 end
 saveas(gcf, strcat('new_krig_error_', regexprep(datestr(datetime('now')), {'[%() :]+', '_+$'}, {'_', ''}), parameter_str, '.pdf'));
 save(strcat('new_krig', regexprep(datestr(datetime('now')), {'[%() :]+', '_+$'}, {'_', ''}), parameter_str, '.mat'))
--- svm predict/new_svm.m
+++ svm predict/new_svm.m
@@ -33,17 +33,18 @@ end
 
 %% plot
 figure('units','normalized','outerposition',[0 0 1 1])  % output graph as full screen
-for kernel_num = 1:4
+for kernel_num = 1:kernel_num
     subplot(2, 2, kernel_num);
-    h = heatmap(error_matrix(:, :, kernel_num));
-    
-    % only for high version MATLAB
-%     h = heatmap(data_set_num_set, train_len_set, error_matrix(:, :, kernel_num));  
-%     h.XLabel = 'data set num set';
-%     h.YLabel = 'train len set';
-%     h.Title = 'Ralative Errors in different parameters'
-    
-    title(strcat('SVM with kernel-', svm_kernel(kernel_num)))
+    if strfind(version, '2015')
+        h = heatmap(error_matrix(:, :, kernel_num));
+        title(strcat('SVM with kernel:', svm_kernel(kernel_num)))
+    elseif strfind(version, '2018')
+        % only for high version MATLAB
+        h = heatmap(data_set_num_set, train_len_set, error_matrix(:, :, kernel_num));  
+        h.XLabel = 'Data set num set';
+        h.YLabel = 'Train len set';
+        h.Title = strcat('Ralative Errors by SVM with kernel: ', svm_kernel(kernel_num));
+    end
 end
 saveas(gcf, strcat('new_svm_error_', regexprep(datestr(datetime('now')), {'[%() :]+', '_+$'}, {'_', ''}), parameter_str, '.pdf'));
 save(strcat('new_svm', regexprep(datestr(datetime('now')), {'[%() :]+', '_+$'}, {'_', ''}), parameter_str, '.mat'))
